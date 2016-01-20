adcf1a115a17b97bc853563a0412ccc795f5e9f2
Quentin MacBook <q.leprat@bjtmail.com>
2016-01-20 10:24:01 +0100

    Init

---
+++ xmazon/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  xmazon
+//
+//  Created by Quentin on 20/01/2016.
+//  Copyright © 2016 Quentin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ xmazon/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  xmazon
+//
+//  Created by Quentin on 20/01/2016.
+//  Copyright © 2016 Quentin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
